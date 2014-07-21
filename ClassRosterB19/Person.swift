916146632d22b1f99c24e7e54d52f63e6a9e8e77
Jeff Gayle <jeff.j.gayle@gmail.com>
2014-07-21 11:46:55 -0700

    initial commit with project

---
+++ ClassRosterB19/Person.swift
@@ -0,0 +1,37 @@
+//
+//  Person.swift
+//  ClassRosterB19
+//
+//  Created by Jeff Gayle on 7/21/14.
+//  Copyright (c) 2014 Jeff Gayle. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    
+//    John Clem
+//    Brad Johnson
+//    Jeff Gayle
+//    Leonardo Lee
+//    Mike Tirenin
+//    Victor Adu
+//    Kirby Shabaga
+//    Collin Atherton
+//    Alex Rice
+//    Dan Hoang
+    
+    var firstName : String
+    var lastName : String
+//    var middleName = "James"
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    func fullName() -> String {
+        return self.firstName + self.lastName
+    }
+    
+}
---
+++ ClassRosterB19Tests/PersonTests.swift
@@ -0,0 +1,48 @@
+//
+//  PersonTests.swift
+//  ClassRosterB19
+//
+//  Created by Jeff Gayle on 7/21/14.
+//  Copyright (c) 2014 Jeff Gayle. All rights reserved.
+//
+
+import UIKit
+import XCTest
+// import ClassRosterB19 - Imports entire module
+
+class PersonTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testPersonCreation() {
+        var person = Person(firstName: "Richard", lastName: "Sherman")
+        XCTAssertNotNil(person, "person can not be created")
+    }
+    
+    func testFirstName() {
+        var person = Person(firstName: "Russell", lastName: "Wilson")
+        XCTAssertNotNil(person.firstName, "person's first name property could not be created")
+    }
+    
+    func testPersonInit() {
+        var person = Person(firstName: "Jeff", lastName: "Gayle")
+        XCTAssertNotNil(person, "person could not be created")
+    }
+    
+    func testFullName() {
+        var first = "Doug"
+        var last = "Baldwin"
+        
+        var person = Person(firstName: first, lastName: last)
+        XCTAssertEqual(person.fullName(), first + last, "full name is not matching")
+    }
+
+}
